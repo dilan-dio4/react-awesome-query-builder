842a24f636ba18d7af9d56315c7d83807f63069f
Dilan Ozkaynak <dilanjames10@gmail.com>
2020-06-02 14:22:00 -0400

    here

--- modules/components/GroupActions.jsx
+++ modules/components/GroupActions.jsx
@@ -1,4 +1,9 @@
 import React, { PureComponent } from 'react';
+import Button from '@material-ui/core/Button'
+import { red } from '@material-ui/core/colors';
+import { DeleteOutlined } from '@ant-design/icons'
+import AddIcon from '@material-ui/icons/Add';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 const groupActionsPositionList = {
   topLeft: 'group--actions--tl',
@@ -10,25 +15,55 @@ const groupActionsPositionList = {
 }
 const defaultPosition = 'topRight';
 
+const trashButtonStyle = {
+  backgroundColor: red.A400,
+  color: 'white',
+  paddingRight: '2px'
+};
 
 export class GroupActions extends PureComponent {
   render() {
-    const {config, addRule, addGroup, canAddGroup, canAddRule, canDeleteGroup, removeSelf} = this.props;
+    const { config, addRule, addGroup, canAddGroup, canAddRule, canDeleteGroup, removeSelf } = this.props;
     const {
-      immutableGroupsMode, addRuleLabel, addGroupLabel, delGroupLabel, groupActionsPosition, 
+      immutableGroupsMode, addRuleLabel, addGroupLabel, delGroupLabel, groupActionsPosition,
       renderButton: Btn, renderButtonGroup: BtnGrp
     } = config.settings;
     const position = groupActionsPositionList[groupActionsPosition || defaultPosition];
 
-    const addRuleBtn = !immutableGroupsMode && canAddRule && <Btn
-      type="addRule" onClick={addRule} label={addRuleLabel} config={config}
-    />;
-    const addGroupBtn = !immutableGroupsMode && canAddGroup && <Btn
-      type="addGroup" onClick={addGroup} label={addGroupLabel} config={config}
-    />;
-    const delGroupBtn = !immutableGroupsMode && canDeleteGroup && <Btn
-      type="delGroup" onClick={removeSelf} label={delGroupLabel} config={config}
-    />;
+    const addRuleBtn = !immutableGroupsMode && canAddRule &&
+      <Button
+        key="group-add-rule"
+        startIcon={<AddIcon />}
+        className="action action--ADD-RULE"
+        onClick={addRule}
+        size="small"
+        variant="outlined"
+        color="primary"
+      >{addRuleLabel}</Button>;
+
+    const addGroupBtn = !immutableGroupsMode && canAddGroup &&
+      <Button
+        key="group-add-group"
+        className="action action--ADD-GROUP"
+        startIcon={<AddCircleOutlineIcon />}
+        onClick={addGroup}
+        size="small"
+        variant="outlined"
+        color="primary"
+      >{addGroupLabel}</Button>;
+
+    const delGroupBtn = !immutableGroupsMode && canDeleteGroup &&
+      <Button
+        key="group-del"
+        type="danger"
+        startIcon={<DeleteOutlined />}
+        className="action action--DELETE"
+        onClick={removeSelf}
+        size="small"
+        variant="contained"
+        style={trashButtonStyle}
+        disableElevation
+      >{delGroupLabel}</Button>;
 
     return (
       <div className={`group--actions ${position}`}>
