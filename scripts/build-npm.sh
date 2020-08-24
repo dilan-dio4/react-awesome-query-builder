7a5c400532c5bf7006759e8b2d51f52b2ec3c7df
Dilan Ozkaynak <dilanjames10@gmail.com>
2020-08-24 10:42:59 -0400

    try

--- scripts/build-npm.sh
+++ scripts/build-npm.sh
@@ -1,5 +1,5 @@
 rm -rf lib
-babel -d lib ./modules
+npx babel -d lib ./modules
 cp modules/index.d.ts lib/index.d.ts
 cp modules/config/antd/index.d.ts lib/config/antd/index.d.ts
 cp modules/components/widgets/antd/index.d.ts lib/components/widgets/antd/index.d.ts
